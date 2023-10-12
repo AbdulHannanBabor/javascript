d3d278509eeba7a86922ca78fd96ff68bf3c64f0
Abdul Hannan <71171189+AbdulHannanBabor@users.noreply.github.com>
2023-10-13 01:08:50 +0600

    new

--- javascript/javascript/14/index.js
+++ javascript/javascript/14/index.js
@@ -5,3 +5,59 @@
 
 // else (num%2 != 0)
 // console.log("odd");
+
+// var num= prompt("enter a number = ");
+
+// // var num=75;
+
+// if (num>=80)
+// console.log("A+ grade");
+
+// else if (num>=70)
+// console.log(" A grade");
+
+// else if (num>=60)
+// console.log("B grade");
+
+
+// else if (num>=50)
+// console.log("C grade");
+
+// else if (num>=40)
+// console.log("D grade");
+
+// else if(num>=31)
+// console.log("E grade");
+
+// else 
+// console.log("fail");
+
+
+// var num= prompt("enter a number = ");
+
+// // var num=75;
+
+// if ( num<=0 ||num>100)
+// console.log("invalid marks");
+
+// else if (num>=80 && num<=100)
+// console.log("A+ grade");
+
+// else if (num>=70 && num <=79)
+// console.log(" A grade");
+
+// else if (num>=60 && num<=69)
+// console.log("B grade");
+
+// else if (num>=50 && num<=69 )
+// console.log("C grade");
+
+// else if (num>=40 && num <=49)
+// console.log("D grade");
+
+// else if(num>=31&& num <=39)
+// console.log("E grade");
+
+// else 
+// console.log("fail");
+
---
+++ javascript/javascript/14/index1.js
@@ -0,0 +1,15 @@
+
+var letter= prompt("enter a letter = ");
+
+letter= letter.toLowerCase();
+
+
+if ( letter=='a' ||letter=='e' || letter=='i' || letter=='o' || letter=='u' );
+
+{
+    console.log("vowel"); 
+}
+else
+{
+    console.log("const");
+}

28d70639851f2dbfb6cca9b5402c4ee28d92a2a9
Abdul Hannan <71171189+AbdulHannanBabor@users.noreply.github.com>
2023-10-12 21:42:36 +0600

    ok

---
+++ javascript/javascript/6th 7th class/class6.js
@@ -0,0 +1,21 @@
+// var num=4;
+// var m= toString(num);
+// console.log(typeof (m));
+
+// var num='88';
+// var m= parseInt(num);
+// console.log(typeof (m));
+
+// var num=88.6765;
+// console.log(num.toFixed(3));
+
+// var num=88.6765;
+// console.log(num.toPrecision(3));
+
+// console.log (typeof(Number('10')));
+
+// console.log (Number(true));
+// console.log (Number(false));
+
+
+

b5dd67f1bd5142507d2a214d2542b17867e1525f
Abdul Hannan <71171189+AbdulHannanBabor@users.noreply.github.com>
2023-10-14 23:16:30 +0600

    Add files via upload

---
+++ javascript/javascript/23/index24.js
@@ -0,0 +1,17 @@
+// var n=5;
+// var r=n*n;
+// console.log(r);
+// creat a function
+function cal(num1,num2){
+    var add=num1+num2;
+    // var sub=num1-num2;
+    // var mul=num1*num2;
+    // var div=num1/num2;
+    // var mod=num1%num2;
+    // console.log(add ,sub ,mul ,div,mod);
+ return add ;
+ 
+}
+ 
+ var result=cal(30,45);
+ console.log(result);

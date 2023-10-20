97565821f352fcd1b5f7d7205c04b73e50c40a02
Abdul Hannan <71171189+AbdulHannanBabor@users.noreply.github.com>
2023-10-20 22:11:58 +0600

    Add files via upload

---
+++ javascript/javascript/32/index.js
@@ -0,0 +1,41 @@
+// var a=Math.sqrt(125);
+// console.log(a);
+
+// var b=Math.pow(5,4);
+// console.log(b);
+
+
+// var c=Math.floor(1.5);
+// console.log(c);
+
+// var d=Math.ceil(1.5);
+// console.log(d);
+
+// var e=Math.round(5.5);
+// console.log(e);
+
+// var f=Math.max(8,3,6,5,6,4,3);
+// console.log(f);
+
+// var g=Math.min(8,3,6,5,6,4,3);
+// console.log(g);
+
+
+
+
+// var h=Math.random()*5;
+
+// h=Math.round(Math.random()*5)+5;
+// console.log(h);
+
+// h=Math.ceil((Math.random()*10)+10);
+// console.log(h);
+
+
+
+
+
+
+
+
+

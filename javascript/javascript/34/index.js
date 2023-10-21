5cc79af31acec2e4a8b3ad8723bf19ea46d3e9f2
Abdul Hannan <71171189+AbdulHannanBabor@users.noreply.github.com>
2023-10-21 22:21:06 +0600

    Add files via upload

---
+++ javascript/javascript/34/index.js
@@ -0,0 +1,40 @@
+// var D =new Date();
+// console.log(D);
+
+// var y=D.getFullYear(D);
+// console.log(y);
+
+// var y=D.getMonth(D);
+// console.log(y);
+
+
+
+
+// class 35
+
+
+// document.getElementById("head1");
+
+// document.getElementById("head1").innerHTML="hello How are you";
+
+
+// var a = document.getElementById("head1");
+// a.innerHTML="hello How are you.";
+
+
+// var b = document.getElementById("title");
+// b.innerHTML="hello How";
+
+
+
+
+//  document.getElementsByTagName('p')[1].innerHTML="hello How a";
+
+
+// var e =document.getElementsByTagName('p')[0];
+// e.innerHTML="hello How a";
+
+
+
+var e =document.getElementsByClassName('title4')[0];
+e.innerHTML="hello How a";

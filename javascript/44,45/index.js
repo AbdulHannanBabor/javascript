7ba8b8d0ea1bd82d7929a2bac1e3fc4c3a60f9de
Abdul Hannan <71171189+AbdulHannanBabor@users.noreply.github.com>
2023-11-01 21:56:27 +0600

    Add files via upload

---
+++ javascript/44,45/index.js
@@ -0,0 +1,44 @@
+
+
+for (var i = 0; i <3; i=i+1){
+    document.querySelectorAll(".btn")[i].addEventListener("click", function(){
+    var text=this.innerHTML;
+    console.log(text);
+
+    audioPlayer(text);
+
+    animation(text);
+    });
+}
+
+
+function audioPlayer(te){
+    switch(te){
+        case "A":
+            var audio = new Audio('audio/1.mp3');
+            audio.play();
+            break;
+        case "B":
+            var audi = new Audio('audio/2.mp3');
+            audi.play();
+            break;
+        case "C":
+            var aud = new Audio('audio/3.mp3');
+            aud.play();
+            break;       
+    }
+}
+
+
+
+function animation(B){
+
+
+    var n=document.querySelector("."+text);
+    n.classList.add("animation");
+
+    setTimeout(function(){
+        n.classList.remove("animation");
+    },1000);
+ 
+}

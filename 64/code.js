30cb1b28db6ba2bae6954d9ac0ce3a324d23e055
Abdul Hannan <71171189+AbdulHannanBabor@users.noreply.github.com>
2023-12-27 23:32:41 +0600

    Add files via upload

---
+++ 64/code.js
@@ -0,0 +1,37 @@
+// let x=11;
+
+// for (let x=1;x<=10;x++){
+
+// // console.log(x);
+// }
+// console.log(x);
+
+// var x=10;
+// var y=40;
+
+// var sum=x+y;
+
+// console.log(x+y);
+
+// function add(x,y){
+// var sum=x+y;
+// console.log(sum);
+// }
+
+// add(10,70);
+
+
+
+
+
+
+
+
+
+var add = (x,y)=>{
+
+let sum=x+y;
+console.log(sum);
+}
+
+add(10,70);

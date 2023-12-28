5fa3f62ba4c25c200893ac23e4da4b73a22c868e
Abdul Hannan <71171189+AbdulHannanBabor@users.noreply.github.com>
2023-12-28 15:44:03 +0600

    Add files via upload

---
+++ 69/code.js
@@ -0,0 +1,15 @@
+// const names=['a', 'b', 'c', 'd', 'e'];
+// for(var i of names) {
+//     console.log(i);
+// }
+
+// const names=['a', 'b', 'c', 'd', 'e'];
+// for(var i of names) {
+//     console.log(i);
+// } 
+
+const names1=['a', 'b', 'c', 'd', 'e'];
+for( i=0;i<3;i++) {
+    // console.log(names1[i]);
+    console.log(names1);
+} 

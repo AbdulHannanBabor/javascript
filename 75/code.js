c7ee883c175898c431bf383bcb3ac8aba7ae350d
Abdul Hannan <71171189+AbdulHannanBabor@users.noreply.github.com>
2023-12-30 00:59:09 +0600

    Add files via upload

---
+++ 75/code.js
@@ -0,0 +1,18 @@
+// let number=[1,2,3,4,5,6,7,8,9];
+
+
+// let firstNumber=number.find(x=> x%2===0);
+// console.log(firstNumber);
+
+const students1=[
+    {id:1,name:3},
+    {id:4,name:33},
+    {id:6,name:332},
+    {id:7,name:2},
+
+];
+
+
+
+
+console.log(students1.find(x=> x.name>50));

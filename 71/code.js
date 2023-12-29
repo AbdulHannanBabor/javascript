3b60fecd4b0fcd6633d742177ed8499aa858a05f
Abdul Hannan <71171189+AbdulHannanBabor@users.noreply.github.com>
2023-12-29 19:01:07 +0600

    Add files via upload

---
+++ 71/code.js
@@ -0,0 +1,33 @@
+// var n=[1,2,3,4,5,6,7,8];
+// console.log(n);
+
+// var square=[];
+// n.forEach(function(i){
+//     square.push(i*i);
+// });
+
+// console.log(square);
+
+
+// var n=[1,2,3,4,5,6,7];
+// console.log(n);
+
+// var square =n.map(function(i){
+//    return i*i;
+// });
+
+// console.log(square);
+
+
+
+
+
+
+var n=[1,20,3,49,56,6,79];
+console.log(n);
+
+var square =n.filter(function(i){
+   return i>10;
+});
+
+console.log(square);
